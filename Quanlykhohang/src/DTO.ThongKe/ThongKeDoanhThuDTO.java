9bf403c2ed8154a3a2e3a90652a58aaf346ec954
Nahobino82 <meltryllis82@gmail.com>
2024-04-13 23:58:21 +0700

    Sua thong ke doanh thu

--- Quanlykhohang/src/DTO.ThongKe/ThongKeDoanhThuDTO.java
+++ Quanlykhohang/src/DTO.ThongKe/ThongKeDoanhThuDTO.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DTO.ThongKe;
+package DTO;
 
 import java.util.Objects;
 
 /**
  *
- * @author Kien
+ * @author Tran Nhat Sinh
  */
 public class ThongKeDoanhThuDTO {
     int thoigian; // nam, thang, ngay
