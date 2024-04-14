f17968fe57d723826058daa559b3cdf738fa895a
kaialison <115174901+kaialison@users.noreply.github.com>
2024-04-14 21:28:22 +0700

    kien heo

--- Quanlykhohang/src/GUI/Log_In.java
+++ Quanlykhohang/src/GUI/Log_In.java
@@ -25,7 +25,7 @@ public class Log_In extends JFrame implements KeyListener {
     JPanel pnlMain, pnlLogIn;
     JLabel lblImage, lbl1, lbl2, lbl3, lbl4, lbl5, lbl6, lbl7;
     InputForm txtUsername, txtPassword;
-    int a = 0;
+    int a = 2;
 
     Color FontColor = new Color(96, 125, 139);
 
